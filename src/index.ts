b78f99be1a9bd66ea4c854d8780c2c6695bfb613
Mihai Plamadeala <mplamadeala@noction.com>
2023-10-22 15:37:38 +0300

    test: add tests

--- src/index.ts
+++ src/index.ts
@@ -10,32 +10,9 @@ const NODE_TYPES = {
   SIMPLE_EXPRESSION: 4,
 } as const
 
-export type AttributeToBeRemoved =
-  | string
-  | { type: 'static' | 'dynamic'; value: string }
-
-export default function createAttributesRemover(attributes: AttributeToBeRemoved[]): NodeTransform {
+export default function createAttributesRemover(attributes: string[]): NodeTransform {
   if (!attributes.length)
     return () => undefined
-
-  const [staticAttributes, dynamicAttributes]: [string[], string[]] = [[], []]
-
-  attributes
-    .forEach((attribute) => {
-      if (typeof attribute === 'string') {
-        staticAttributes.push(attribute)
-        dynamicAttributes.push(attribute)
-
-        return
-      }
-
-      if (attribute.type === 'static') {
-        staticAttributes.push(attribute.value)
-        return
-      }
-
-      dynamicAttributes.push(attribute.value)
-    })
 
   return (node): void => {
     if (node.type !== NODE_TYPES.ELEMENT)
@@ -44,10 +21,10 @@ export default function createAttributesRemover(attributes: AttributeToBeRemoved
     node.props = node.props
       .filter((prop) => {
         if (prop.type === NODE_TYPES.ATTRIBUTE)
-          return !staticAttributes.includes(prop.name)
+          return !attributes.includes(prop.name)
 
         if (prop.type === NODE_TYPES.DIRECTIVE && prop.arg?.type === NODE_TYPES.SIMPLE_EXPRESSION)
-          return !dynamicAttributes.includes(prop.arg.content)
+          return !attributes.includes(prop.arg.content)
 
         return true
       })
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,55 @@
-import { describe } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { compileTemplate } from '@vue/compiler-sfc'
+import type { NodeTransform } from '@vue/compiler-core'
+import createAttributesRemover from '../src'
 
-describe.skip('vue-plugin-remove-attributes')
+describe('createAttributesRemover', () => {
+  const template
+    = `<template>
+      <div>
+        <header data-test="header-test">
+          Header
+        </header>
+        <section :data-test="'section-content'">
+          <ul>
+            <li
+              :data-test="'item-text'"
+              v-text="'text'"
+            />
+          </ul>
+          <div data-meta="not-secure-meta">
+            Meta 
+          </div>
+        </section>
+        <footer dataTest="footer-test">
+          Footer
+        </footer>
+      </div>
+    </template>`
+
+  const parse = (removeParameters: NodeTransform) => {
+    return compileTemplate({
+      id: 'TestComponent.vue',
+      filename: 'TestComponent.vue',
+      source: template,
+      compilerOptions: {
+        nodeTransforms: [removeParameters],
+      },
+    }).code
+  }
+
+  describe('empty attributes', () => {
+    it('should not change anything', () => {
+      const code = parse(createAttributesRemover([]))
+
+      expect(code).toMatchSnapshot()
+    })
+  })
+  describe('with attributes', () => {
+    it('should remove all data-test and data-meta', () => {
+      const code = parse(createAttributesRemover(['data-test', 'data-meta']))
+
+      expect(code).toMatchSnapshot()
+    })
+  })
+})
